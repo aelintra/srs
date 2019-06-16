1fa1531126218c2b798e6b6ff465fc6a82afadf5
CoCo <admin@aelintra.com>
2019-06-16 21:39:57 +0100

    built general getter

--- sysAPI.src/processGets.php
+++ sysAPI.src/processGets.php
@@ -1,45 +1,68 @@
 <?php
 
+require_once __DIR__ . "/model/getClass";
+/*
+    GET processors
+ */ 
+
         $app->get('/endpoint', function ($request, $response, $args) {
-                try {   
+            $getter = new getController($this->db,$this->response);
+            return $this->response->withJson($getter->getAll('endpoint'));
+        }
+
+        $app->get('/endpoint/{pkey}', function ($request, $response, $args) {
+            $getter = new getController($this->db,$this->response);
+            return $getter->getId('endpoint',$args['pkey']);
+        } 
+
+        $app->get('/endpoint/{pkey}/{column}', function ($request, $response, $args) {
+            $getter = new getController($this->db,$this->response);
+            return $getter->getId('endpoint',$args['pkey'],$args['column']);
+        }   
+
+/*
+        $app->get('/endpoint', function ($request, $response, $args) {
+            try {   
                 $sth = $this->db->prepare("SELECT * FROM endpoint order by mac");
                 $sth->execute();
                 $endpoint = $sth->fetchAll();
-        } catch (exception $e) {
+            } catch (exception $e) {
                 $result ['reason'] = $e->getMessage();
-            return $this->response->withJson($result,500);        
-        }         
-        return $this->response->withJson($endpoint);
+                return $this->response->withJson($result,500);        
+            }            
+            return $this->response->withJson($endpoint);
         });
         
         $app->get('/endpoint/{mac}', function ($request, $response, $args) {
-                try {
+            try {
                 $sth = $this->db->prepare("SELECT * FROM endpoint where mac=?");
                 $sth->execute(array($args['mac']));
                 $endpoint = $sth->fetchAll();
-        } catch (exception $e) {
+            } catch (exception $e) {
                 $result ['reason'] = $e->getMessage();
-            return $this->response->withJson($result,500);        
-        } 
-        if (empty($endpoint)) {
+                return $this->response->withJson($result,500);        
+            } 
+            if (empty($endpoint)) {
                 $result ['reason'] = "MAC " . $args['mac'] . " Not Found";
-            return $this->response->withJson($result,404);   
-        }              
-        return $this->response->withJson($endpoint);
+                return $this->response->withJson($result,404);   
+            }              
+            return $this->response->withJson($endpoint);
         });
 
         $app->get('/endpoint/{mac}/{column}', function ($request, $response, $args) {
-                try {
+            try {
                 $sth = $this->db->prepare("SELECT " . $args['column'] . " FROM endpoint where mac=?");
                 $sth->execute(array($args['mac']));
                 $endpoint = $sth->fetchAll();
-        } catch (exception $e) {
+            } catch (exception $e) {
                 $result ['reason'] = $e->getMessage();
-            return $this->response->withJson($result,500);        
-        }
-        if (empty($endpoint)) {
+                return $this->response->withJson($result,500);        
+            }
+            if (empty($endpoint)) {
                 $result ['reason'] = "MAC " . $args['mac'] . "/" . $args['column'] . " Not Found";
-            return $this->response->withJson($result,404);   
-        }            
-        return $this->response->withJson($endpoint);
+                return $this->response->withJson($result,404);   
+            }            
+            return $this->response->withJson($endpoint);
         });
+*/
+
