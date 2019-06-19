170c0b1f9ae8e20fd2c52ac8a65a02601b549b4f
CoCo <admin@aelintra.com>
2019-06-19 10:33:25 +0100

    provisioning fit

--- db/createMysql.sql
+++ db/createMysql.sql
@@ -16,17 +16,17 @@ CREATE TABLE IF NOT EXISTS customer (
     pkey VARCHAR(64) NOT NULL DEFAULT 'system',             /* Customer name/number */ 
     parent VARCHAR(64),                                     /* parent Foreign key */ 
 
-    dflt_ep_user VARCHAR(32) DEFAULT 'user',                /* this is for the phone browser */
-    dflt_ep_user_pass VARCHAR(32) DEFAULT 'myuserpass',     /* this is for the phone browser */
-    dflt_ep_admin_pass VARCHAR(32) DEFAULT 'myadminpass',   /* this is for the phone browser */ 
+    ep_user VARCHAR(32) DEFAULT 'user',                /* this is for the phone browser */
+    ep_user_pass VARCHAR(32) DEFAULT 'myuserpass',     /* this is for the phone browser */
+    ep_admin_pass VARCHAR(32) DEFAULT 'myadminpass',   /* this is for the phone browser */ 
 
     dflt_redirecturl VARCHAR(2047),     /* default redirect target */     
     dflt_sip_reg VARCHAR(2047),         /* default SIP Registration URL */
 
-    ldap_base VARCHAR(2047),            /* default LDAP base */
-    ldap_host VARCHAR(2047),            /* default LDAP host */
-    ldap_pass VARCHAR(2047),            /* default LDAP pwd */    
-    ldap_user VARCHAR(2047),            /* default LDAP user */
+    ldapbase VARCHAR(2047),            /* default LDAP base */
+    ldaphost VARCHAR(2047),            /* default LDAP host */
+    ldappass VARCHAR(2047),            /* default LDAP pwd */    
+    ldapuser VARCHAR(2047),            /* default LDAP user */
 
     z_created DATETIME DEFAULT CURRENT_TIMESTAMP,
     z_updated TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -83,14 +83,16 @@ CREATE TABLE IF NOT EXISTS endpoint (
     customer VARCHAR(64),                   /* Customer foreign key */
     template VARCHAR(64),                   /* Template chain base name */
     device VARCHAR(64),                     /* Autofill device model */
-
+    
     displayname VARCHAR(32),                /* Phone display name */ 
     devicemodel VARCHAR(64),                /* Autofill device model */
+
     mode TINYINT(1) DEFAULT 1,              /* 0 => Inactive, 1 => Redirect, 2 => Provision */
+    provision TEXT,                         /* Autofill from pkey template */
     redirecturl VARCHAR(2047),              /* redirect target */       
-    sip_account VARCHAR(32),                /* SIP peer name (extension)  */ 
-    sip_pass VARCHAR(32),                   /* SIP password */
-    sip_reg VARCHAR(2047),                  /* SIP Registration URL */ 
+    sipaccount VARCHAR(32),                 /* SIP peer name (extension)  */ 
+    sippass VARCHAR(32),                    /* SIP password */
+    sipurl VARCHAR(2047),                   /* SIP Registration URL */ 
     vendor VARCHAR(32),                     /* Autofill from MAC lookup on MAC DB */  
 
     z_created DATETIME DEFAULT CURRENT_TIMESTAMP,
