a50a33d4e14e7da8d652398d93f85f5b6073c81e
CoCo <admin@aelintra.com>
2019-06-21 08:48:59 +0100

    provisioning fit

--- db/createMysql.sql
+++ db/createMysql.sql
@@ -12,6 +12,10 @@ GRANT ALL PRIVILEGES
 
 use macdb;
 
+/* Customer Head of chain or customerPBX instance */
+/* create one customer head of chain for each customer and */
+/* one or more customerPBX children */
+
 CREATE TABLE IF NOT EXISTS customer (
     pkey VARCHAR(64) NOT NULL DEFAULT 'system',        /* Customer name/number */ 
     parent VARCHAR(64),                                /* parent Foreign key */ 
@@ -20,8 +24,12 @@ CREATE TABLE IF NOT EXISTS customer (
     ep_user_pass VARCHAR(32) DEFAULT 'myuserpass',     /* this is for the phone browser */
     ep_admin_pass VARCHAR(32) DEFAULT 'myadminpass',   /* this is for the phone browser */ 
 
-    dflt_redirecturl VARCHAR(2047),    /* default redirect target */     
-    dflt_sipurl VARCHAR(2047),         /* default SIP Registration URL */
+    provurl VARCHAR(2047),        /* provisioning url to set in the phone */
+    redirecturl VARCHAR(2047),    /* default HTTP/HTTPS redirect target for this PBX */
+                                  /* You can override this at the endpoint if you have */
+                                  /* multiple provisioning instances */
+
+    sipurl VARCHAR(2047),         /* SIP Registration URL for this PBX */
 
     ldapbase VARCHAR(2047),            /* default LDAP base */
     ldaphost VARCHAR(2047),            /* default LDAP host */
@@ -80,21 +88,22 @@ CREATE TABLE IF NOT EXISTS device (
 
 CREATE TABLE IF NOT EXISTS endpoint (
     pkey VARCHAR(12) NOT NULL,              /* MAC address */
-    customer VARCHAR(64),                   /* Customer foreign key */
-    template VARCHAR(64),                   /* Template chain base name */
-    device VARCHAR(64),                     /* Autofill device model */
-    
+    customer VARCHAR(64),                   /* Customer PBX foreign key */
+    customtemplate VARCHAR(64),             /* Custom template overrides vendor template */
+    device VARCHAR(64),                     /* Autofill device model */   
+
     displayname VARCHAR(32),                /* Phone display name */ 
     mode TINYINT(1) DEFAULT 1,              /* 0 => Inactive, 1 => Redirect, 2 => Provision */
-    provision TEXT,                         /* Autofill from pkey template */
-    redirecturl VARCHAR(2047),              /* redirect target */       
-    sipaccount VARCHAR(32),                 /* SIP peer name (extension)  */ 
+    provision TEXT,                         /* Autofill from template */
+    redirecturl VARCHAR(2047),              /* redirect target (overrides pbx redirect) */       
+    sipaccount VARCHAR(32),                 /* SIP peer name (extension number)  */ 
     sippass VARCHAR(32),                    /* SIP password */
-    sipurl VARCHAR(2047),                   /* SIP Registration URL */ 
     vendor VARCHAR(32),                     /* Autofill from MAC lookup on MAC DB */  
+                                            /* Base template will be copied from this */
+                                            /* you can override it with a customtemplate  */
 
     z_created DATETIME DEFAULT CURRENT_TIMESTAMP,
-    z_firstseen DATETIME,                   /* first request date */
+    z_firstseen DATETIME,                   /* first request date  */
     z_lastseen DATETIME,                    /* latest request date */
     z_lastloc VARCHAR(32),                  /* last request origin */
     z_updated TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -106,7 +115,7 @@ CREATE TABLE IF NOT EXISTS endpoint (
     ON DELETE SET NULL
     ON UPDATE CASCADE,
 
-    FOREIGN KEY (template) REFERENCES template(pkey)
+    FOREIGN KEY (customtemplate) REFERENCES template(pkey)
     ON DELETE SET NULL
     ON UPDATE CASCADE,
 
