bce97bf24b156754be62df41041ce7fa577c2aa6
CoCo <admin@aelintra.com>
2019-06-22 11:26:45 +0100

    provisioning fit

--- sysAPI.src/routePuts.php
+++ sysAPI.src/routePuts.php
@@ -4,17 +4,7 @@ require_once __DIR__ . "/model/putController";
 require_once __DIR__ . "/model/helperClass";
 
         $app->put('/endpoint', function ($request, $response, $args) {
-        	$input = $request->getParsedBody();
-    		if (empty($input['customer'])) {
-        		$result ['reason'] = "NULL customer KEY";
-            	return $this->response->withJson($result,400);      	
-    		}
-    		$helper = new modelHelper();
-            $input['vendor'] = $helper->getVendorFromMac($input['pkey']);
-            if (empty($input['vendor'])) {
-        		$result ['reason'] = "MAC (key) is not a supported vendor";
-            	return $this->response->withJson($result,400);                	
-            }    		    		    		    		
+        	$input = $request->getParsedBody();		    		    		    		
             $putter = new putController($this->db,$input,$this->response);
             return $putter->put('endpoint');
         });
